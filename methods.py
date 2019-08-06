e465192a05c66626ddff558c94b1c931622ee20d
Thome <thomeneto@hotmail.com>
2019-08-06 16:27:34 -0300

    methods v2

--- methods.py
+++ methods.py
@@ -1,3 +1,4 @@
+import numpy as np
 # A = [[a11, a12],[a21, a22]] mxn
 # A[row] = [a21, a22]
 # x = [x1, x2]
@@ -9,11 +10,12 @@ def jacobi(n,A,b,XO,TOL,N_iter):
     while (k <= N_iter):
 
     	for i in range(0,n):
-    		aux = [A[i][j] * XO[j] + b[i] for j in range(0,n) if j != i]
+    		aux = [A[i,j] * XO[j] + b[i] for j in range(0,n) if j != i]
     		soma = -sum(aux)
-    		x[i] = (1/A[i][i]) * soma
+    		x[i] = (1/A[i,i]) * soma
 
-    	if abs(x - XO) < TOL:
+    	norm = np.linalg.norm(x - XO)
+        if norm < TOL:
     		print("The procedure was successful")
     		return x
 
@@ -29,14 +31,14 @@ def jacobi(n,A,b,XO,TOL,N_iter):
 
 n = 2
 
-A = [[2,1],
-     [5,7]]
+A = np.array([[2,1],
+              [5,7]])
 
-b = [11,13]
+b = np.array([11,13])
 
-XO = [1,1]
+XO = np.array([1,1])
 
-TOL = []
+TOL = 0
 
 N_iter = 25
 
@@ -48,11 +50,12 @@ def gauss_seidel(n,A,b,XO,TOL,N_iter):
     while (k <= N_iter):
 
         for i in range(0,n):
-            aux1 = -sum([A[i][j]*x[j] for j in range(0,i-1)])
-            aux2 = -sum([A[i][j]*XO[j] + b[i] for j in range(i+1,n)])
-            x[i] = (1/A[i][i]) * (aux1+aux2)
+            aux1 = -sum([A[i,j]*x[j] for j in range(0,i-1)])
+            aux2 = -sum([A[i,j]*XO[j] + b[i] for j in range(i+1,n)])
+            x[i] = (1/A[i,i]) * (aux1+aux2)
 
-        if abs(x - XO) < TOL:
+        norm = np.linalg.norm(x - XO)
+        if norm < TOL:
             print("The procedure was successful")
             return x
 
@@ -70,12 +73,13 @@ def SQR(n,A,b,XO,w,TOL,N_iter):
     while (k <= N_iter):
 
         for i in range(0,n):
-            aux1 = -sum([A[i][j]*x[j] for j in range(0,i-1)])
-            aux2 = -sum([A[i][j]*XO[j] + b[i] for j in range(i+1,n)])
-            aux = (1/A[i][i]) * (w*(aux1+aux2))
+            aux1 = -sum([A[i,j]*x[j] for j in range(0,i-1)])
+            aux2 = -sum([A[i,j]*XO[j] + b[i] for j in range(i+1,n)])
+            aux = (1/A[i,i]) * (w*(aux1+aux2))
             x[i] = (1-w)*XO[i] + aux
 
-        if abs(x - XO) < TOL:
+        norm = np.linalg.norm(x - XO)
+        if norm < TOL:
             print("The procedure was successful")
             return x
 
